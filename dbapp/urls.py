471fd546eb68d051b5040253e9e30387d27be8f7
khsrla9806 <kimhunsope@naver.com>
2022-11-23 19:43:46 +0900

    refactor: 사용하지 않는 url 정리 및 주석제거

--- dbapp/urls.py
+++ dbapp/urls.py
@@ -4,7 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    # path('login', views.login, name = 'login'),
-    # path('signin', views.signin, name = 'signin'),
     path('post', views.post, name = 'post')
 ]
--- dbapp/views.py
+++ dbapp/views.py
@@ -3,11 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
-def login(request):
-    return render(request, 'login.html')
-
-def signin(request):
-    return render(request, 'signin.html')
-
 def post(request):
     return render(request, 'post.html')
