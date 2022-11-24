310fe432d4224c7e1ae953180083edc72015f22d
khsrla9806 <kimhunsope@naver.com>
2022-11-24 14:11:31 +0900

    feat: 대학 주변 시설 모델 구현 완료

--- dbapp/admin.py
+++ dbapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Facility
 
 # Register your models here.
+admin.site.register(Facility)
--- dbapp/models.py
+++ dbapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Facility(models.Model):
+    opt1= "카페"
+    opt2= "음식점"
+    opt3= "술집"
+    CHOICES = ((opt1, "카페"), (opt2, "음식점"),(opt3, "술집"))
+    
+    name = models.CharField(max_length=30, null=False)
+    category = models.CharField(choices=CHOICES, max_length=50, null=True, blank=True)
+    content = models.TextField(null=True)
+    tel_number = models.CharField(max_length=20, null=True)
+    image = models.ImageField(upload_to='facility/', default='/static/images/hanseo_logo.png', null=True, blank=True)
+    url = models.URLField(null=True)
+    
+    def __str__(self):
+        return self.name
