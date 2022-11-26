11bccf5832b451e53e1e1da14a3e90241286ca02
김경준 <kkzoone@gimgyeongjun-ui-MacBookAir.local>
2022-11-26 23:08:00 +0900

    feat: 검색

--- search/views.py
+++ search/views.py
@@ -1,16 +1,49 @@
 from importlib.resources import contents
 from django.shortcuts import render
 from django.db.models import Q 
-from dbapp.models import Club;
+from django.db import connection
+from dbapp.models import Club,Facility;
 
 
 
 def searchResult(request):
     if 'kw' in request.GET:
         query = request.GET.get('kw')
-        clubs = Club.objects.all().filter(
-            Q(name__icontains=query) |
-            Q(image__icontains=query) |
-            Q(content__icontains=query)
+        club = Club.objects.all().filter(
+            Q(name__icontains=query) 
         )
-    return render(request, 'search.html', {'query':query, 'clubs':clubs})
+        facility = Facility.objects.all().filter(
+            Q(name__icontains=query) 
+        )
+        reseult = club.union(facility)
+    return render(request, 'search.html', {'query':query, 'reseult':reseult})
+
+# def searchResult(request):
+#     if request.method=='GET':
+#         searchs =[]
+#         try:
+#             target = request.GET['kw']
+#             print("1")
+#             cursor = connection.cursor()
+#             sql = "SELECT name, content, image FROM hanseobase.dbapp_club WHERE name=(%s);"
+#             # sql = "SELECT name, content FROM hanseobase.dbapp_facility WHERE name LIKE '(%s)' UNION SELECT name, content FROM hanseobase.dbapp_club WHERE name LIKE '(%s)';"
+#             print("")
+#             result = cursor.execute(sql,[target,])
+#             print("3")
+#             datas = cursor.fetchall()
+#             print("4")
+#             connection.commit()
+#             connection.close()
+
+#             # searchs =[]
+#             for data in datas:
+#                 row = {
+#                     'name' : data[0],
+#                     'content': data[1],
+#                 }
+#                 searchs.append(row)
+#         except:
+#             connection.rollback()
+#             print("찾고자 하는 정보가 없습니다.")
+
+#         return render(request,'search.html',{'searchs':searchs})
